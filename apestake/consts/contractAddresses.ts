5769637380b11f041953e1a47b14400df129f1c3
Liquorhands <breyerkevin2019@gmail.com>
2023-08-13 08:23:53 +0200

    1

---
+++ apestake/consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x2F3883AbDBB28410ae9C75DBb98cfD96f44945e8";
+export const tokenContractAddress =
+  "0x5cB5cbdb7500cf3cd73c9A230f624729Cff217ef";
+export const stakingContractAddress =
+  "0x0b403cff15aca9c69d0d70F12AD724acB6fc2213";
---
+++ apestake/pages/_app.tsx
@@ -0,0 +1,86 @@
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme, Box } from "@chakra-ui/react";
+import "../styles/globals.css"; // Import your global CSS file here
+import Head from "next/head";
+import { metamaskWallet, walletConnect,  coinbaseWallet, } from "@thirdweb-dev/react";
+import Onboard from '@web3-onboard/core';
+import injectedModule from '@web3-onboard/injected-wallets';
+import { CronosBeta } from "@thirdweb-dev/chains";
+
+
+
+const MAINNET_RPC_URL = 'https://evm.cronos.org/';
+const injected = injectedModule();
+
+const onboard = Onboard({
+  wallets: [injected],
+  chains: [
+    {
+      id: '0x25',
+      token: 'CRO',
+      label: 'Cronos Mainnet',
+      rpcUrl: MAINNET_RPC_URL
+    }
+  ],
+  appMetadata: {
+    name: 'Apes Staking',
+    icon: 'https://ipfs.flair.finance/ipfs/QmWjizft2Pok8LNKLHaTLw911qNPsEJwWaBYPV9PiTv2D1',
+    description: 'NFT Staking App'
+  }
+});
+
+
+
+
+const backgroundImageUrl = "https://ipfs.flair.finance/ipfs/QmWjizft2Pok8LNKLHaTLw911qNPsEJwWaBYPV9PiTv2D1"; // Replace with the actual background image URL
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        background: `url(${backgroundImageUrl}) no-repeat fixed center center`, // Set the background image URL here
+        backgroundSize: "cover",
+      },
+    },
+  },
+});
+
+const activeChainId = "0x25";
+const activeChain = "CronosBeta";
+
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider
+      supportedWallets={[
+        walletConnect(),
+        metamaskWallet(),
+        coinbaseWallet(), 
+      ]}
+      activeChain={CronosBeta}
+      clientId={process.env.TW_CLIENT_ID}
+    >
+      <ChakraProvider theme={theme}>
+        <Head>
+          {/* Add your custom text here */}
+          <title>Kingdom Apes NFT Staking</title>
+        </Head>
+        {/* Wrap the background image with the Box component */}
+        <Box
+          w="100%"
+          h="100vh"
+          _hover={{
+            background: "hsl(243, 4.9%, 18.8%)",
+          }}
+        >
+          <Component {...pageProps} />
+        </Box>
+      </ChakraProvider>
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
+
